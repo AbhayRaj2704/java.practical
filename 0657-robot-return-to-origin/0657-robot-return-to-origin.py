ae552ea57f56f9db4804c7434e325c727bf0e6f6
Abhay Raj <abhayraj0304@gmail.com>
2026-08-09 16:10:14 +0530

    Time: 0 ms (100%), Space: 19.3 MB (54.42%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -0,0 +1,7 @@
+class Solution:
+    def judgeCircle(self, moves: str) -> bool:
+        if moves.count('L') == moves.count('R') and moves.count('D')== moves.count('U'):
+            return True
+        
+        else:
+            return False
