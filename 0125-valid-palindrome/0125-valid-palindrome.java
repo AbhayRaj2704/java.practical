e21a6cd97f16edd8b61213da5b22e5021dc2adb3
Abhay Raj <abhayraj0304@gmail.com>
2026-09-12 18:43:17 +0530

    Time: 16 ms (12.92%), Space: 52 MB (5.1%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,33 +1,18 @@
 class Solution {
+    public static boolean rev(int i, String s){
+        if (i>=s.length()/2){
+            return true;
+        }
+        if(s.charAt(i) != s.charAt(s.length()-i-1)){
+            return false;
+        }
+        return rev(i+1,s);
+    }
     public boolean isPalindrome(String s) {
-
-        s = s.toLowerCase();
-        StringBuilder sb = new StringBuilder();
-
-        // Step 1: clean string
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-
-            if (ch >= 'a' && ch <= 'z') {
-                sb.append(ch);
-            } 
-            else if (ch >= '0' && ch <= '9') {
-                sb.append(ch);
-            }
-        }
-
-        // Step 2: check palindrome
-        int left = 0;
-        int right = sb.length() - 1;
-
-        while (left <= right) {
-            if (sb.charAt(left) != sb.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-
-        return true;
+         s = s.toLowerCase();
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        return rev(0,s);
+       
+     
     }
 }
